f67bdc388a9c31ef513f81785e1eeee18691455e
Sultan Mukhtarov <suli419956@gmail.com>
2025-01-12 09:45:24 +0600

    created fixtures with some data's and login of 2 different acc's.

--- api/fixtures.ts
+++ api/fixtures.ts
@@ -1,14 +1,48 @@
 import config from "./config";
 import mongoose from "mongoose";
+import User from "./models/User";
+import {randomUUID} from "crypto";
+import Task from "./models/Task";
 
 const run = async () => {
     await mongoose.connect(config.database);
     const db = mongoose.connection;
-    try {
 
+    try {
+        await db.dropCollection("users");
+        await db.dropCollection("tasks");
     } catch (error) {
-        console.log('Skipping drop.....')
+        console.log("Skipping drop...");
     }
+
+    const [Suli, Sultan] = await User.create(
+        {
+            username: "Suli",
+            password: "123",
+            token: randomUUID(),
+        },
+        {
+            username: "Sultan",
+            password: "qwerty123",
+            token: randomUUID(),
+        }
+    );
+
+    await Task.create([
+        {
+            user: Sultan._id,
+            title: "The first Task",
+            description: "The task that I have to do",
+            status: "in_progress",
+        },
+        {
+            user: Suli._id,
+            title: "The task From another acc",
+            description: "The task that I already done",
+            status: "done",
+        },
+    ]);
+    await db.close();
 };
 
 void run();
